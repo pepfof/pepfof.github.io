20eb4476f23d5aa42f21d4c10b2dae2c8460294c
pepfof <artemiodro@gmail.com>
2021-06-08 22:04:32 +0300

    Incremental Minecraft Update

--- minecraft/generate&upload.sh
+++ minecraft/generate&upload.sh
@@ -1,6 +1,6 @@
 find . -type f > mine.txt &
 find . -type d > dirs.txt 
-sed -i "s ./ \ g" mine.txt
+cat mine.txt | tr “/” “\\” > mine1.txt
 sed -i "s / \ g" mine.txt
 sed -i "s ./ \ g" dirs.txt
 sed -i "s / \ g" dirs.txt
