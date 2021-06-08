a732e9639ac669302bd3e46785185bda4749cf5e
pepfof <artemiodro@gmail.com>
2021-06-08 22:09:15 +0300

    Incremental Minecraft Update

--- minecraft/generate&upload.sh
+++ minecraft/generate&upload.sh
@@ -1,10 +1,10 @@
 find . -type f > mine1.txt &
 find . -type d > dirs1.txt 
-cat mine1.txt | tr “./” '\\' > mine2.txt
-cat mine2.txt | tr “/” '\\' > mine.txt
+sed -i 's ./ \\ g' mine1.txt 
+cat mine1.txt | tr './' '\\' > mine2.txt
 cat dirs1.txt | tr “./” '\\' > dirs2.txt
 cat dirs2.txt | tr “/” '\\' > dirs.txt
-rm mine1.txt mine2.txt dirs1.txt dirs2.txt
+#rm mine1.txt mine2.txt dirs1.txt dirs2.txt
 git add -A
 git commit -m "Incremental Minecraft Update"
 git push
